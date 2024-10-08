032ecc7ed37749e87b408590f12374362f31da6f
TOKY <>
2024-10-08 17:00:18 +0200

    Ajustement assignation des catégories par joueurs

--- exemple_tri.py
+++ exemple_tri.py
@@ -1,31 +1,20 @@
 # Utilisation de pandas pour une manipulation plus simple et plus flexible des données contrairement à csv
 import pandas as pd
 
-def tri_joueurs_par_categories(fichier_csv):
+def tri_joueurs_par_categories(fichier_joueurs, fichier_categories):
 
     # Lecture du fichier csv avec les joueurs
-    joueurs = pd.read_csv(fichier_csv)
+    joueurs = pd.read_csv(fichier_joueurs)
 
     if joueurs.empty:
         print("Le fichier CSV est vide ou non valide.")
         return None
     
-    # Définition des catégories d'âge et de poids
-    # Chaque catégorie a une plage d'âges et des intervalles de poids correspondants
-    categories_age_poids = {
-        'Poussin': {'age_min': 6, 'age_max': 7, 'poids': [(0,20), (20,25), (25,30), (30,35), (35,40), (40,45)]},
-        'Pupille': {'age_min': 8, 'age_max': 9, 'poids': [(0,25), (25,30), (30,35), (35,40), (40,45), (45,50)]},
-        'Benjamin': {'age_min': 10, 'age_max': 11, 'poids': [(0,30), (30,35), (35,40), (40,45), (45,50), (50,55)]},
-        'Minime': {'age_min': 12, 'age_max': 13, 'poids': [(0,35), (35,40), (40,45), (45,50), (50,55), (55,60), (60, 65)]},
-        'Cadet': {'age_min': 14, 'age_max': 15, 'poids': [(0,42), (42,47), (47,54), (54,60), (60,70)]},
-        'Junior': {'age_min': 16, 'age_max': 17, 'poids': [(0,48), (48,53), (53,59)]},
-        'Espoir': {'age_min': 18, 'age_max': 20, 'poids': [(0,50), (50,55), (55,61), (61,68), (68,75), (75,84)]},
-        'Senior': {'age_min': 18, 'age_max': 34, 'poids': [(0,50), (50,55), (55,61), (61,68), (68,75), (75,84)]},
-        'V1': {'age_min': 35, 'age_max': 45, 'poids': [(0,55), (55,61), (61,68), (68,75), (75,84)]},
-        'V2': {'age_min': 46, 'age_max': 55, 'poids': [(0,55), (55,61), (61,68), (68,75), (75,84)]},
-        'V3': {'age_min': 56, 'age_max': 65, 'poids': [(0,55), (55,61), (61,68), (68,75), (75,84)]},
-    }
+    categories = pd.read_csv(fichier_categories)
 
+    if categories.empty:
+        print("Le fichier CSV est vide ou non valide.")
+        return None
 
     # ATTRIBUTION CATEGORIE D'AGE ET DE POIDS POUR CHAQUES JOUEURS
 
@@ -41,14 +30,11 @@ def tri_joueurs_par_categories(fichier_csv):
     def assigner_categorie(joueur):
         age, poids = joueur['age'], joueur['poids']
 
-        for cat, details in categories_age_poids.items():
-            # Vérifier si l'âge du joueur est dans les limites de la catégorie
-            if details['age_min'] <= age <= details['age_max']:
-                # Vérifier si le poids du joueur est dans les limites de la catégorie
-                for poids_min, poids_max in details['poids']:
-                    if poids_min <= poids <= poids_max:
-                        # Retourne la catégorie correspondante
-                        return cat, f'{poids_min}-{poids_max}kg'
+        for _, row in categories.iterrows():
+            if row['age_min'] <= age <= row['age_max']:
+                if row['poids_min'] <= poids <= row['poids_max']:
+                    return row['categorie'], f"{row['poids_min']}-{row['poids_max']}kg"
+                
         return None, None
 
     # On applique la fonction assigner_categorie à chaque ligne du DataFrame "joueurs", issue de la lecture du fichier csv
@@ -58,9 +44,9 @@ def tri_joueurs_par_categories(fichier_csv):
 
     # Affichage des joueurs par catégories d'âge et de poids
     for (cat_age, cat_poids), group in joueurs.groupby(['categorie_age', 'categorie_poids']):
-        print(f"Joueurs dans la catégorie {cat_age} {cat_poids}:")
-        print(group[['nom', 'prenom', 'poids', 'age', 'club']])
+        print(f"Joueur(s) dans la catégorie {cat_age} {cat_poids}:")
+        print(group[['nom', 'prenom', 'poids', 'age', 'club']], "\n")
         
     return joueurs
 
-joueur = tri_joueurs_par_categories('joueur.csv')
+joueur = tri_joueurs_par_categories('joueur.csv', 'categorie.csv')
