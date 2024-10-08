cffef49fd0d777f4b2fd75d4d51e956a388c31d7
TOKY <>
2024-10-08 09:40:28 +0200

    Exemple tableau catégories et poids

--- exemple_tri.py
+++ exemple_tri.py
@@ -9,4 +9,11 @@ def tri_joueurs_par_categories(fichier_csv):
         print("Le fichier CSV est vide ou non valide.")
         return None
     
-
+    # Définition des catégories d'âge et de poids
+    # Chaque catégorie a une plage d'âges et des intervalles de poids correspondants
+    categories_age_poids = {
+        'Minime': {'age_min': 10, 'age_max': 12, 'poids': [(0,20), (20,25), (25,30)]},
+        'Junior': {'age_min': 13, 'age_max': 16, 'poids': [(0,40), (40,50), (50,60)]},
+        'Cadet': {'age_min': 17, 'age_max': 19, 'poids': [(0,60), (60,70), (70,80)]},
+        'Senior': {'age_min': 20, 'age_max': 100, 'poids': [(0,80), (80,90), (90,100)]},
+    }
