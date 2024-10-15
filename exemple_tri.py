b491e83b41e74957ec0ac3414219c5e0471937d4
TOKY <>
2024-10-15 09:26:08 +0200

    Save

--- exemple_tri.py
+++ exemple_tri.py
@@ -20,14 +20,13 @@ def tri_joueurs_par_categories(fichier_joueurs, fichier_categories):
 
     # ATTRIBUTION CATEGORIE D'AGE ET DE POIDS POUR CHAQUES JOUEURS
 
-    # On commence par extraire l'âge et le poids du joueur.
-    # Pour chaque catégorie d'âge définie dans le dictionnaire categories_age_poids, on vérifie si l'âge
-    # du joueur se situe dans les limites de la catégorie.
-    # Si c'est le cas, on parcourt les plages de poids associées à cette catégories pour déterminer si 
-    # le poids du joueur est compris dans l'une d'elles.
-    # Si les 2 conditions sont réunis, on retourne la catégorie d'âge et la plage de poids sous la forme
-    # "20-25kg"
-    # Si il n'y a aucune correspondance, on retourne None, None
+    """ On commence par extraire l'âge et le poids du joueur.
+     Pour chaque catégorie d'âge définie dans le dictionnaire categories_age_poids, on vérifie si l'âge
+     du joueur se situe dans les limites de la catégorie.
+     Si c'est le cas, on parcourt les plages de poids associées à cette catégories pour déterminer si 
+     le poids du joueur est compris dans l'une d'elles.
+     Si les 2 conditions sont réunis, on retourne la catégorie d'âge et la plage de poids sous la forme
+     "20-25kg". Si il n'y a aucune correspondance, on retourne None, None """
 
     def assigner_categorie(joueur):
         age, poids = joueur['age'], joueur['poids']
@@ -39,9 +38,10 @@ def tri_joueurs_par_categories(fichier_joueurs, fichier_categories):
                 
         return None, None
 
-    # On applique la fonction assigner_categorie à chaque ligne du DataFrame "joueurs", issue de la lecture du fichier csv
-    # On le réalise ligne par ligne "axis=1" et non par colonne "axis=0"
-    # Si la fonction renvoie une liste ou un tuple pour chaque ligne,  'expand' va étendre la sortie sur plusieurs colonnes 
+    """ On applique la fonction assigner_categorie à chaque ligne du DataFrame "joueurs", issue de la lecture du fichier csv
+     On le réalise ligne par ligne "axis=1" et non par colonne "axis=0"
+     Si la fonction renvoie une liste ou un tuple pour chaque ligne,  'expand' va étendre la sortie sur plusieurs colonnes """
+
     joueurs[['categorie_age', 'categorie_poids']] = joueurs.apply(assigner_categorie, axis=1, result_type='expand')
 
     # Calcul du nombre de joueurs par club dans chaque catégorie
