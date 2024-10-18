ea44af5004284a60669c16d8226eced4fe30eecb
Frostheda <nabil49.sabry49@gmail.com>
2024-10-18 15:39:56 +0200

    docstring pour la classe Graphe

--- Class/Graphe.py
+++ Class/Graphe.py
@@ -1,22 +1,33 @@
-#Class/Pile.py
-from collections import defaultdict
+from collections import defaultdict # Importation pour utiliser des dictionnaires avec des valeurs par défaut
 
 class Graphe:
-    "La classe représente un graphe à l'aide d'une liste d'adjacence."
+    """
+    Représente un graphe à l'aide d'une liste d'adjacence.
+    Les arêtes sont orientées avec une capacité associée.
+    """
 
     def __init__(self):
+        """
+        Initialise un graphe vide avec une liste d'adjacence.
+        """
         self.liste_adjacence = defaultdict(dict)
     
     def ajouter_arete(self, sommet_depart, sommet_arrivee, capacite):
         """
-        Ajout d'une arête avec une capacité donnée.
-        :param sommet_depart: Le sommet de départ de l'arête
-        :param sommet_arrivee: Le sommet d'arriver de l'arête
-        :param capacite: La capacité de l'arête
+        Ajoute une arête entre deux sommets avec une capacité.
+        
+        :param sommet_depart: Sommet de départ
+        :param sommet_arrivee: Sommet d'arrivée
+        :param capacite: Capacité de l'arête
         """
-
         self.liste_adjacence[sommet_depart][sommet_arrivee] = capacite
         self.liste_adjacence[sommet_arrivee][sommet_depart] = 0
 
     def obtenir_voisins(self, sommet):
+        """
+        Retourne les voisins d'un sommet.
+        
+        :param sommet: Le sommet dont on veut les voisins
+        :return: Liste des voisins du sommet
+        """
         return self.liste_adjacence[sommet].keys()
